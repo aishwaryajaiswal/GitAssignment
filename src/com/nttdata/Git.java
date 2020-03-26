8f6a0df968c00e106891e0f82f86c0a679aa8c86
Aishwarya Jaiswal <ishuaj10@gmail.com>
2020-03-26 15:26:48 +0530

    Second Commit-- Scanner

---
+++ src/com/nttdata/Git.java
@@ -0,0 +1,15 @@
+package com.nttdata;
+
+import java.util.Scanner;
+
+public class Git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		System.out.println("enter your name");
+		String name = s.nextLine();
+		System.out.println("My Name Is " + name);
+	}
+
+}
